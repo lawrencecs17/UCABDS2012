670faf8ff8579b0b901a6b4b74975b5985912859
ricardo portela <ricky663@gmail.com>
2012-05-26 00:17:52 -0430

    probando el commit

--- miOrquidea/grails-app/controllers/miorquidea/CalificacionController.groovy
+++ miOrquidea/grails-app/controllers/miorquidea/CalificacionController.groovy
@@ -14,6 +14,7 @@ class CalificacionController {
 	/**
 	* Metodo que lista todos las calificaciones registrados en el sistema
 	* Debe ser solicitado mediante una peticion GET
+	* ........
 	*/
     def listarTodos ={
         		
