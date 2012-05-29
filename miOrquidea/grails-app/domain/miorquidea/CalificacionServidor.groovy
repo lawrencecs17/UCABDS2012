585e049f6a7daca70c8d59a3e5da9fa090644b5a
ricardo portela <ricky663@gmail.com>
2012-05-28 22:13:02 -0430

    subieendo clase CalificacionServidor para asignar la cantidad de like, 	        dislike y comentados de un comentario

---
+++ miOrquidea/grails-app/domain/miorquidea/CalificacionServidor.groovy
@@ -0,0 +1,25 @@
+package miorquidea
+
+import java.util.Date;
+import grails.converters.XML
+
+/**
+* Clase destinada a dar respuesta al cliente de las operaciones fallidas
+* y de advertencia por parte del servidor con respecto a la calificacion
+* de los comentario
+* @author Ricardo Portela
+*
+*/
+class CalificacionServidor {
+
+	String Like
+	String Dislike
+	String Comentados
+
+    static constraints = {
+    }
+	
+	/**
+	* Mensaje cuando la consulta a la Base de Datos no devuelve documentos
+	*/
+}
